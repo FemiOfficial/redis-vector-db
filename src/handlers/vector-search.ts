8dcb42e9254c520e607fcd170b500cea4f0fdc9c
Femi <alayesanmifemi@gmail.com>
2025-08-06 01:02:07 +0100

    added vector search

---
+++ src/handlers/vector-search.ts
@@ -0,0 +1,33 @@
+import { redisClient } from "redis/connect";
+import {
+  getTransformerPipeline,
+  embeddingPipelingOptions,
+} from "../transformer/pipeline";
+
+const __RESPONSES_INDEX__ = "responses_idx";
+
+export const vectorSearch = async (query: string): Promise<any> => {
+  const pipe = await getTransformerPipeline();
+
+  const queryEmbedding = (await pipe(query, embeddingPipelingOptions)).data;
+
+  const searchResults = await redisClient.ft.search(
+    __RESPONSES_INDEX__,
+    "*=>[KNN 3 @response_embedding $QUERY_EMBEDDING AS score]",
+    {
+      PARAMS: {
+        QUERY_EMBEDDING: Buffer.from(Array.from(queryEmbedding)),
+      },
+      RETURN: [
+        "score",
+        "$.question",
+        "$.response",
+        "$.policy_tag",
+        "$.responsible_individuals",
+        "$.relevant_document_names",
+      ],
+      DIALECT: 2,
+    }
+  );
+  return searchResults;
+};
--- src/transformer/pipeline.ts
+++ src/transformer/pipeline.ts
@@ -1,9 +1,9 @@
-import { pipeline } from "@xenova/transformers";
+import { FeatureExtractionPipelineOptions, pipeline } from "@xenova/transformers";
 
 export const getTransformerPipeline = async () =>
   await pipeline("feature-extraction", "Xenova/all-distilroberta-v1");
 
-export const embeddingPipelingOptions = {
+export const embeddingPipelingOptions: FeatureExtractionPipelineOptions = {
   pooling: "mean",
   normalize: true,
 };
